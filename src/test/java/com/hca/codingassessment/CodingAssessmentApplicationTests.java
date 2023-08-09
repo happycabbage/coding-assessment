fdbba0eaed3cd75a6c9bfddd7ff640e59ab585ec
Eric Cobb <ericdcobb@gmail.com>
2023-08-09 15:18:28 -0600

    Inititial commit - scaffolding for spring boot project

---
+++ src/test/java/com/hca/codingassessment/CodingAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hca.codingassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
